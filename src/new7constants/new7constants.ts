bb3376a710051cf681268c89555d2bf62354d2a1
RohanNayak909 <75715641+RohanNayak909@users.noreply.github.com>
2022-11-07 11:25:15 +0530

    Release1.1.1

---
+++ src/new7constants/new7constants.ts
@@ -0,0 +1,60 @@
+export const News7_CONSTANTS = {
+    LOGIN_MODE: {
+        normal: 'N',
+        google: 'G',
+        twitch: 'T',
+        discort: 'D',
+        facebook: 'F'
+    },
+    LOOKUPS: {
+        adsize: 'ADS_SIZE',
+        mobileCountryCode: 'CNTRY_CODE_MB',
+        entertainment:'Entertainment',
+        business :'Business',
+        technology:'Technology',
+        crime:'Crime',
+        politics:'Politics',
+        sports:'Sports',
+        education:'Education',
+        nation:'Nation',
+        automobile:'Automobile',
+        health:'Health',
+        blog:'Blog',
+        women:'Women',
+        science:'Science',
+        twin_City:'Twin_City',
+        campus_muse: 'Campus_Muse',
+        odisha:'Odisha',
+        none: 'None'
+    },
+    ERRORS: {
+        global_error: 'Something went wrong, please try after sometime!'
+    },
+    SUCCESS: {
+        global_success: 'Success'
+    },
+    FLAG: {
+        INSERT: 'I',
+        UPDATE: 'U',
+        DELETE: 'D',
+        ADD: 'A',
+        REMOVE: 'R',
+        START_PHASE: 'SP',
+        END_PHASE: 'EP'
+    },
+    DELETION_FLAG: {
+        USER: 'USERS',
+        GAME: 'GAMES',
+        TOURNAMENT: 'TOUR',
+        TEAM: 'TEAM',
+        EVENT: 'EVENT'
+    },
+    SCHEDULE_FLAG: {
+        ROUND_SCHEDULE: 'RU',
+        MATCH_SCHEDULE: 'MU',
+        GROUP_UPDATE: 'GU',
+        TOSS_UPDATE: 'TS'
+    },
+    LANGUAGES: ['en', 'es', 'ca', 'ar']
+
+};

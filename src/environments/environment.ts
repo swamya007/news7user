8331e13c299ef4a4f51504dc2414f506e57df0e5
swamya <swamyaswain1996@gmail.com>
2022-11-11 22:05:29 +0530

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   POST_URL: 'http://localhost:4200/post/',
-  BASE_URL: 'http://localhost:8072/prameya/api',
+  BASE_URL: 'http://192.168.5.121:8072/prameya/api',
   CUSTOMER_ID : 2,
 };
 
@@ -16,4 +16,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.  

6556cdad6829e354dc53f99164866908c588fdbd
swamya <swamyaswain1996@gmail.com>
2023-03-28 14:25:06 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   POST_URL: 'https://dev.prameya.com/post/',
   BASE_URL: 'https://api-dev.prameyanews.com/prameya/api',
-  CUSTOMER_ID : 2,
-  CUSTOMER_NAME :'PRAMEYA_ODIA',
-  PLATFORM_BASEURL: 'https://dev.prameya.com',
+  CUSTOMER_ID: 2,
+  CUSTOMER_NAME: 'PRAMEYA_ODIA',
+  PLATFORM_BASEURL: 'https://dev.prameyanews.com/',
 };

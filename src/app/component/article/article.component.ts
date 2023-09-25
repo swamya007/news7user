8ecde546560c2477e8547f662332b28285147b3f
RohanNayak909 <75715641+RohanNayak909@users.noreply.github.com>
2023-09-25 17:07:29 +0530

    seo

--- src/app/component/article/article.component.ts
+++ src/app/component/article/article.component.ts
@@ -84,7 +84,7 @@ export class ArticleComponent implements OnInit {
 
             this.news.post_content_sanitized =
               this.sanitizer.bypassSecurityTrustHtml(this.news.post_content);
-            this.Title.setTitle(this.news.post_title);
+            this.Title.setTitle(this.news.seo_title);
             let imgURL = this.news.guid;
             let newsTitle = this.news.post_title;
             let newsDesc = this.news.post_content;
@@ -94,13 +94,13 @@ export class ArticleComponent implements OnInit {
             let tags = [
               { name: 'twitter:card', content: 'summary' },
               { name: 'twitter:image', content: imgURL },
-              { name: 'twitter:title', content: this.news.seo_title },
+              { name: 'twitter:title', content: newsTitle },
               { name: 'twitter:description', content: newsDesc },
               { name: 'og:type', content: 'article' },
-              { name: 'og:title', content: this.news.seo_title },
+              { name: 'og:title', content: this.news.newsTitle },
               { name: 'og:description', content: newsDesc },
               { name: 'description', content: newsDesc },
-              { name: 'seo:title', content: this.news.seo_title },
+              { name: 'seo:title', content: this.news.newsTitle },
               { name: 'og:url', content: postURL },
               { name: 'image', content: imgURL },
               { name: 'og:image', content: imgURL },
@@ -146,7 +146,7 @@ export class ArticleComponent implements OnInit {
           this.news.post_content_sanitized =
             this.sanitizer.bypassSecurityTrustHtml(this.news.post_content);
 
-          this.Title.setTitle(this.news.post_title);
+          this.Title.setTitle(this.news.seo_title);
           let imgURL = this.news.guid;
           let newsTitle = this.news.post_title;
           let newsDesc = this.news.post_content;
@@ -157,10 +157,10 @@ export class ArticleComponent implements OnInit {
           let tags = [
             { name: 'twitter:card', content: 'summary' },
             { name: 'twitter:image', content: imgURL },
-            { name: 'twitter:title', content: this.news.seo_title },
+            { name: 'twitter:title', content: newsTitle },
             { name: 'twitter:description', content: newsDesc },
             { name: 'og:type', content: 'article' },
-            { name: 'og:title', content: this.news.seo_title },
+            { name: 'og:title', content: this.news.newsTitle },
             { name: 'og:description', content: newsDesc },
             { name: 'description', content: newsDesc },
             { name: 'seo:title', content: this.news.seo_title },
@@ -466,7 +466,7 @@ export class ArticleComponent implements OnInit {
   }
 
   updateSEO_Tags() {
-    this.Title.setTitle(this.news.post_title);
+    this.Title.setTitle(this.news.seo_title);
     let imgURL = this.news.guid;
     let newsTitle = this.news.post_title;
     let newsDesc = this.news.post_content;
@@ -477,12 +477,12 @@ export class ArticleComponent implements OnInit {
     let tags = [
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:image', content: imgURL },
-      { name: 'twitter:title', content: this.news.seo_title },
+      { name: 'twitter:title', content: this.news.newsTitle },
       { name: 'twitter:description', content: newsDesc },
-      { name: 'og:title', content: this.news.seo_title },
+      { name: 'og:title', content: this.news.newsTitle },
       { name: 'og:description', content: newsDesc },
       { name: 'description', content: newsDesc },
-      { name: 'seo:title', content: this.news.seo_title },
+      { name: 'seo:title', content: this.news.newsTitle },
       { name: 'og:url', content: postURL },
       { name: 'image', content: imgURL },
       { name: 'og:image', content: imgURL },
